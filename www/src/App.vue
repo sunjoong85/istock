d6724608a4e53e6b3f1b24272e0acaafeacbd49b
Sunjoong Kim <sunjoong85@gmail.com>
2018-06-24 16:45:10 +0900

    update vue material

--- www/src/App.vue
+++ www/src/App.vue
@@ -3,35 +3,38 @@
     <md-app md-mode="fixed">  <!-- fixed reveal -->
       <md-app-toolbar class="md-primary">
         <!-- viewport에 따라 사라지도록 하기-->
-        <md-button class="md-icon-button" @click="menuVisible = !menuVisible">
+
+        <md-button class="md-icon-button" v-if="!menuVisible" @click="menuVisible = !menuVisible">
           <md-icon>menu</md-icon>
         </md-button>
-        <span class="md-title">SJ Playground</span>
+
+        <span class="md-title">Sunjoong85 Playground</span>
       </md-app-toolbar>
 
-      <md-app-drawer :md-active.sync="menuVisible">  <!-- permanent-->
+      <md-app-drawer :md-permanent="drawerType" :md-active.sync="menuVisible">  <!----><!-- permanent-->
         <md-toolbar class="md-transparent" md-elevation="0">Navigation</md-toolbar>
 
         <md-list>
           <md-list-item>
-            <md-icon>move_to_inbox</md-icon>
-            <span class="md-list-item-text">Stock Price</span>
+            <md-icon>show_chart</md-icon>
+            <span class="md-list-item-text">Investment Factors</span>
+          </md-list-item>
+          <md-list-item>
+            <md-icon>bar_chart</md-icon>
+            <span class="md-list-item-text">D3</span>
           </md-list-item>
 
           <md-list-item>
-            <md-icon>send</md-icon>
-            <span class="md-list-item-text">Sent Mail</span>
+            <md-icon>highlight</md-icon>
+            <span class="md-list-item-text">Experiments</span>
           </md-list-item>
 
-          <md-list-item>
-            <md-icon>delete</md-icon>
-            <span class="md-list-item-text">Trash</span>
-          </md-list-item>
 
           <md-list-item>
-            <md-icon>error</md-icon>
-            <span class="md-list-item-text">Spam</span>
+            <md-icon>perm_identity</md-icon>
+            <span class="md-list-item-text">About</span>
           </md-list-item>
+
         </md-list>
       </md-app-drawer>
 
@@ -54,25 +57,55 @@
   export default {
     name: 'App',
     data() {
-       return {
-         menuVisible : false
-       }
+      return {
+        menuVisible: true,
+        drawerType: 'clipped'
+      }
     },
+
     components: {
       TraderTrend, DramExchange
+    },
+
+    mounted() {
+
+      var e = document.documentElement,
+        g = document.getElementsByTagName('body')[0],
+        x = window.innerWidth || e.clientWidth || g.clientWidth,
+        mobile = false;
+
+      if (x <= 480) {
+        // console.log("mobile size");
+        mobile = true;
+        this.menuVisible = false;
+        this.drawerType = null;
+      }
+
     }
+
   }
 </script>
 
 <style lang="scss">
+
+  .page-container {
+    height: 100vh
+  }
+
   .md-app {
-    max-height: 100%;
+    /*// max-height: 100%;*/
+    height: 100%;
     border: 1px solid rgba(#000, .12);
+  }
+
+  .md-app-toolbar {
+    height: 5vh;
   }
 
   // Demo purposes only
   .md-drawer {
-    width: 230px;
+    width: 25vw;  //todo media query
+    height: 100vh; //max height : screen size
     max-width: calc(100vw - 125px);
   }
 </style>
